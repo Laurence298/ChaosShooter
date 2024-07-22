d05946c05437eb5b795e67f29d3bfbffce826579
Anoone <72642342+Anooneee@users.noreply.github.com>
2024-07-22 02:00:19 -0600

    l

--- Script/PauseMenu.gd
+++ Script/PauseMenu.gd
@@ -1,10 +1,8 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	pass
+
 func _input(event):
 	if Input.is_action_just_pressed("esc"):
 		self.visible = true
