8746d1e45fd04866c85a87db9260360d8bfb00b3
nsriv531 <nsriv531@mtroyal.ca>
2024-07-22 11:38:05 -0600

    changes to main?

--- Scenes/Camera2D.gd
+++ Scenes/Camera2D.gd
@@ -1,13 +1,24 @@
 extends Camera2D
 
 @onready var player := get_tree().get_first_node_in_group("player")
+var previous_position : Vector2
+
+func _ready():
+	previous_position = global_position
 
 func _physics_process(delta):
 	var mouse_pos = get_global_mouse_position()
+	
+	# Store the current position before updating
+	previous_position = global_position
 	
 	offset.x = snapped((mouse_pos.x - global_position.x) / (1920.0 / 2.0) * 150, 0.5)
 	offset.y = snapped((mouse_pos.y - global_position.y) / (1080.0 / 2.0) * 150, 0.5)
 	
 	self.global_position = player.global_position
 	
+	force_update_scroll()
+
+func revert_to_previous_position():
+	global_position = previous_position
 	force_update_scroll()
--- Script/PauseMenu.gd
+++ Script/PauseMenu.gd
@@ -1,10 +1,15 @@
 extends Control
 
+var is_paused := false
+
+@onready var camera := get_parent().get_node("Camera2D") # Adjust the path to your Camera2D node if necessary
+
 func _ready():
-	pass
+	self.visible = false # Ensure the pause menu is hidden initially
 
 func _input(event):
 	if Input.is_action_just_pressed("esc"):
-		self.visible = true
-		get_tree().paused = true
+		is_paused = !is_paused # Toggle the pause state
+		self.visible = is_paused
+		get_tree().paused = is_paused
 	pass
