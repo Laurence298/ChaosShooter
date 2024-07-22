b0e5a04a4d46ad55765944c776c7ea0f1b23aa69
nsriv531 <nsriv531@mtroyal.ca>
2024-07-22 02:30:32 -0600

    main enu stuff

--- Scenes/Menus/WinMenu.gd
+++ Scenes/Menus/WinMenu.gd
@@ -1,11 +1,5 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_resume_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes//main.tscn") # Replace with function body.
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
