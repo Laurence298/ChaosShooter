c128a88848725248bccf3432699306f2ce6e48a8
nsriv531 <nsriv531@mtroyal.ca>
2024-07-21 21:16:18 -0600

    menu stuff

---
+++ Script/DeathMenu.gd
@@ -0,0 +1,19 @@
+extends Button
+
+
+@onready var heatdeath = $CombustText
+@onready var gundeath = $GunText
+@onready var dogdeath = $DogText
+
+func _on_heat_death():
+	heatdeath.show = true
+	
+func _on_gun_death():
+	gundeath.show = true	
+	
+func _on_dog_death():
+	dogdeath.show = true	
+
+func _on_try_again_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes//PlaceHolder.tscn") # Replace with function body.
+	pass # Replace with function body.
--- Script/MainMenu.gd
+++ Script/MainMenu.gd
@@ -5,4 +5,5 @@ func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes//PlaceHolder.tscn") # Replace with function body.
 
 func _on_quit_button_pressed():
-	get_tree().quit() # Replace with function body.
+	get_tree().quit()
+	pass # Replace with function body.
